6b841e38a78861512a2203df9b55189915960be4
Cintuza <cinbiblio@yahoo.com.ar>
2020-04-18 22:22:28 -0300

    Modificación de comentarios a javadoc

--- src/ar/edu/unq/po2/tp3/Counter.java
+++ src/ar/edu/unq/po2/tp3/Counter.java
@@ -20,7 +20,9 @@ public class Counter {
 		this.getNumbers().add(number);
 	}
 	
-	// Devuelve la cantidad de numeros pares que posee la lista
+	/**
+	 *  Devuelve la cantidad de numeros pares que posee la lista
+	 */
 	public int getEvenOccurrences() {
 		int evenOccurrences = 0;
 		for (int i=0; i < this.getNumbers().size(); i++) {
@@ -32,7 +34,9 @@ public class Counter {
 		return evenOccurrences;
 	}
 
-	// Devuelve la cantidad de numeros impares que posee la lista
+	/**
+	 *  Devuelve la cantidad de numeros impares que posee la lista
+	 */
 	public int getOddOccurrences() {
 		int oddOccurrences = 0;
 		for (int i=0; i < this.getNumbers().size(); i++) {
@@ -44,7 +48,9 @@ public class Counter {
 		return oddOccurrences;
 	}
 
-	// Devuelve true si el numero number es par; devuelve false si es impar
+	/**
+	 *  Devuelve true si el numero number es par; devuelve false si es impar
+	 */
 	private boolean isEven(Integer number) {
 		boolean isEven = true;
 		if (number.intValue() % 2 == 1) {
@@ -53,7 +59,9 @@ public class Counter {
 		return isEven;
 	}
 
-	// Devuelve la cantidad de multiplos de otherNumber que contiene la lista
+	/**
+	 *Devuelve la cantidad de multiplos del numero otherNumber que contiene la lista
+	 */
 	public int getMultiplesOfAmount(int otherNumber) {
 		int multiplesOfAmount = 0;
 		for (int i=0; i < this.getNumbers().size(); i++) {
@@ -65,7 +73,12 @@ public class Counter {
 		return multiplesOfAmount;
 	}
 
-	// Devuelve true si el numero number es multiplo del numero otherNumber
+	/**
+	 * Chequea si un numero es multiplo de otro
+	 * @param number posible multiplo, de tipo Integer
+	 * @param otherNumber el divisor
+	 * @return {@code true} si {@code number} es multiplo de {@code otherNumber}
+	 */
 	private boolean isMultipleOf(Integer number, int otherNumber) {
 		boolean isMultiple = false;
 		if (number.intValue() % otherNumber == 0) {
