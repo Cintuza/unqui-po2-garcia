3e96f385efe57afee153c8617942a060dde05c97
Cintuza <cinbiblio@yahoo.com.ar>
2020-05-22 19:37:44 -0300

    Separacion de IServidor en IServidorBasico e IServidorAvanzado

---
+++ src/ar/edu/unq/po2/tp6/ClienteCorreo/IServidorAvanzado.java
@@ -0,0 +1,11 @@
+package ar.edu.unq.po2.tp6.ClienteCorreo;
+
+public interface IServidorAvanzado {
+	
+	public float tazaDeTransferencia();
+
+	public void resetear();
+	
+	public void realizarBackUp();
+
+}
