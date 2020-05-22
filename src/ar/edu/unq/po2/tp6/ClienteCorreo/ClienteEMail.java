b5ca2b1c594da1d73c03d3212387e903938d9fc8
Cintuza <cinbiblio@yahoo.com.ar>
2020-05-22 19:43:29 -0300

    Division de responsabilidades con CasillaDeCorreo. Variable servidor pasa de tipo ServidorPop a IServidorBasico

--- src/ar/edu/unq/po2/tp6/ClienteCorreo/ClienteEMail.java
+++ src/ar/edu/unq/po2/tp6/ClienteCorreo/ClienteEMail.java
@@ -1,21 +1,19 @@
 package ar.edu.unq.po2.tp6.ClienteCorreo;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class ClienteEMail {
 	
-	 ServidorPop servidor;
+	 IServidorBasico servidor;
 	 String nombreUsuario;
 	 String passusuario;
-	 ArrayList<Correo> inbox;
-	private ArrayList<Correo> borrados;
+	 CasillaDeCorreos casillaDeCorreos;
 	
-	public ClienteEMail(ServidorPop servidor, String nombreUsuario, String pass){
+	public ClienteEMail(IServidorBasico servidor, String nombreUsuario, String pass){
 		this.servidor=servidor;
 		this.nombreUsuario=nombreUsuario;
 		this.passusuario=pass;
-		this.inbox = new ArrayList<Correo>();
-		this.borrados = new ArrayList<Correo>();
+		this.casillaDeCorreos = new CasillaDeCorreos();
 		this.conectar();
 	}
 	
@@ -23,25 +21,9 @@ public class ClienteEMail {
 		this.servidor.conectar(this.nombreUsuario,this.passusuario);
 	}
 	
-	public void borrarCorreo(Correo correo){
-		this.inbox.remove(correo);
-		this.borrados.remove(correo); // es borrados.add(correo)?
-	}
-	
-	public int contarBorrados(){
-		return this.borrados.size();
-	}
-	
-	public int contarInbox(){
-		return this.inbox.size();
-	}
-	
-	public void eliminarBorrado(Correo correo){
-		this.borrados.remove(correo);
-	}
-	
 	public void recibirNuevos(){
-		this.servidor.recibirNuevos(this.nombreUsuario, this.passusuario);
+		List<Correo> correosNuevos = this.servidor.recibirNuevos(this.nombreUsuario, this.passusuario);
+		casillaDeCorreos.almacenarNuevos(correosNuevos);
 	}
 	
 	public void enviarCorreo(String asunto, String destinatario, String cuerpo){
