2398ef80402d711ce2698e3bffc4b095b29bdbd1
Cintuza <cinbiblio@yahoo.com.ar>
2020-07-06 08:34:45 -0300

    version original

---
+++ src/ar/edu/unq/po2/tp11/cuenta/HistorialMovimientos.java
@@ -0,0 +1,7 @@
+package ar.edu.unq.po2.tp11.cuenta;
+
+public interface HistorialMovimientos {
+
+	void registrarMovimiento(String descripcion, Integer monto);
+
+}
---
+++ src/ar/edu/unq/po2/tp11/cuenta/Notificador.java
@@ -0,0 +1,6 @@
+package ar.edu.unq.po2.tp11.cuenta;
+
+public interface Notificador {
+	void notificarNuevoSaldoACliente(CuentaBancaria cuentaBancaria);
+
+}
